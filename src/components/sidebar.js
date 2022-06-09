c9009dcbd8d6acd2c253866740658d8be9a3931c
honganh <nguyenhonganh@zen8labs.com>
2022-06-09 10:45:36 +0700

    add navbar function

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,32 +5,31 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  Avatar,
+  Stack,
+  Typography,
 } from "@mui/material";
 
-import MailIcon from "@mui/icons-material/Mail";
-import StarIcon from '@mui/icons-material/Star';
-import ShareIcon from '@mui/icons-material/Share';
-import DeleteIcon from '@mui/icons-material/Delete'
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import AudiotrackIcon from "@mui/icons-material/Audiotrack";
 
 
 const ListSideBar = [
   {
-  title:'All Files',
-  icon: <MailIcon/>
+    title: "Home",
+    icon: <HomeIcon fontSize="large" />
   },
   {
-    title: 'Starred',
-    icon: <StarIcon/>
+    title: "Search",
+    icon: <SearchIcon />,
   },
   {
-    title: 'Shared',
-    icon: <ShareIcon/>
-  },
-  {
-    title: 'Deleted',
-    icon: <DeleteIcon/>
+    title: "Library",
+    icon: <LibraryMusicIcon />,
   }
-]
+];
 
 const Sidebar = () => {
   const drawerWidth = 240;
@@ -43,19 +42,30 @@ const Sidebar = () => {
         "& .MuiDrawer-paper": {
           width: drawerWidth,
           boxSizing: "border-box",
-          position:'absolute',
-          top:'45px'
+          color: "white",
+          background: "black",
         },
       }}
       variant="permanent"
       anchor="left"
     >
-      <List sx={{ paddingTop: "30px", fontSize:'16px' }}>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        mt={3}
+      >
+        <Avatar src="https://i.pinimg.com/564x/80/04/f5/8004f5a0d01462dbbeab0053878f0817.jpg" />
+        <Typography sx={{ fontFamily: "fantasy", fontSize: "30px" }}>
+   
+          Mellifluous
+        </Typography>
+      </Stack>
+      <List sx={{ paddingTop: "30px", fontSize: "20px" }}>
         {ListSideBar.map((item) => (
-          <ListItem button key={item.title}>
-            <ListItemIcon>
-              {item.icon}
-            </ListItemIcon>
+          <ListItem button key={item.title} >
+            <ListItemIcon sx={{color: 'white'}}>{item.icon}</ListItemIcon>
             <ListItemText primary={item.title} />
           </ListItem>
         ))}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Grid, Stack } from '@mui/material';
-import Navbar from '../components/navbar'
+import { Stack } from '@mui/material';
+
 import Sidebar from '../components/sidebar'
 
 const Dashboard = () => {
   return (
-    <Stack direction="column" spacing ={2}>
-   <Navbar/>
     <Stack direction="row" >
       <Sidebar/>
-      <div>Hello World</div>
+      
     </Stack>
   
-     
-   
-   </Stack>
   )
 }
 
